799a735b673d9425bbc76cfaf589b8112c597f68
Prashant Kumar <prashant.aman9@gmail.com>
2022-09-12 18:37:57 +0530

    Create state to hold our array of numbers and Map over the state numbers array to generate our array of Die elements and render those in place of our manually-written 10 Die elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Die from "./Die";
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -8,21 +11,13 @@ function App() {
     }
     return newDice;
   }
-  console.log(allNewDice());
+  const diceElements = dice.map((die,index) => <Die key={index} value={die} />)
+
   return (
     <div className="app-container">
       <div className="app">
         <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
+          {diceElements}
         </div>
       </div>
     </div>
