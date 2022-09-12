bb8a9538be4d579fd30f8b2ca42a220260f06934
Prashant Kumar <prashant.aman9@gmail.com>
2022-09-12 18:33:24 +0530

    add fxn to return array of 10 random integers from 1 to 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import Die from './Die'
+import React from "react";
+import Die from "./Die";
 function App() {
-
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  console.log(allNewDice());
   return (
-    <div className='app-container'>
-      <div className='app'>
+    <div className="app-container">
+      <div className="app">
         <div className="dice-container">
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
