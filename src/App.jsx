025505e7a058594591a9556dcbaa8e38adcc92e1
Prashant Kumar <prashant.aman9@gmail.com>
2022-09-12 20:19:35 +0530

    Update the `rollDice` function to not roll the held dies

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { nanoid } from "nanoid";
 
 function App() {
   /**
-   * Challenge: Update the `holdDice` function to flip
-   * the `isHeld` property on the object in the array
-   * that was clicked, based on the `id` prop passed
-   * into the function.
+   * Challenge: Update the `rollDice` function to not just roll
+   * all new dice, but instead to look through the existing dice
+   * to NOT role any that are being `held`.
    *
-   * Hint: as usual, there's > 1 way to accomplish this.
-   * I'll be using `dice.map()` and checking for the `id`
-   * of the die to determine which one to flip `isHeld` on,
-   * but you can do whichever way makes the most sense to you.
+   * Hint: this will look relatively similiar to the `holdDice`
+   * function below. When creating new dice, remember to use
+   * `id: nanoid()` so any new dice have an `id` as well.
    */
   const [dice, setDice] = React.useState(allNewDice());
-
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -29,7 +33,11 @@ function App() {
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   function holdDice(id) {
@@ -52,6 +60,11 @@ function App() {
   return (
     <div className="app-container">
       <div className="app">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="dice-container">{diceElements}</div>
         <button className="roll-dice" onClick={rollDice}>
           Roll
