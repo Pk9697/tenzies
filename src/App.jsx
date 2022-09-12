c5271cb0051af7eeaaac8ac24d6ba27aec402364
Prashant Kumar <prashant.aman9@gmail.com>
2022-09-12 20:28:07 +0530

    log `you won` if all values are same and held

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,35 @@ import { nanoid } from "nanoid";
 
 function App() {
   /**
-   * Challenge:
-   * 1. Add new state called `tenzies`, default to false. It
-   *    represents whether the user has won the game yet or not.
-   * 2. Add an effect that runs every time the `dice` state array
-   *    changes. For now, just console.log("Dice state changed").
+   * Challenge: Check the dice array for these winning conditions:
+   * 1. All dice are held, and
+   * 2. all dice have the same value
+   *
+   * If both conditions are true, set `tenzies` to true and log
+   * "You won!" to the console
    */
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
 
   React.useEffect(
     function () {
-      console.log("Dice state changed");
+      const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            console.log("You won!") 
+        }
+      // let win = true;
+      // let checkDie = dice[0].value;
+      // for (let i = 0; i < dice.length; i++) {
+      //   let die = dice[i];
+      //   if (!die.isHeld || die.value != checkDie) {
+      //     win = false;
+      //     break;
+      //   }
+      // }
+      // setTenzies(win);
     },
     [dice]
   );
@@ -27,7 +44,7 @@ function App() {
       id: nanoid(),
     };
   }
-  
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
